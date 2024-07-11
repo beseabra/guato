4b2beb47c21c46f9b5ed61961e5fef256a59d30a
berseabra <bernardo.seabra@cubos.io>
2024-07-11 15:56:58 -0300

    Call Option

--- components/Moleculas/Orders.tsx
+++ components/Moleculas/Orders.tsx
@@ -1,7 +1,8 @@
 import { Draft } from "@/app/(tabs)/explore";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
+import { addHours, format, parse } from "date-fns";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Linking, StyleSheet, Text, View } from "react-native";
 
 interface OrderProps {
   filteredDrafts: Draft[];
@@ -30,34 +31,93 @@ const Order: React.FC<OrderProps> = ({ filteredDrafts }) => {
 
   return (
     <>
-      {filteredDrafts.map((draft) => (
-        <View key={draft.id} style={styles.draftContainer}>
-          <View style={styles.nameReference}>
-            <View style={styles.iconContainer}>
-              <MaterialCommunityIcons
-                name="hair-dryer"
-                size={24}
-                color="black"
+      {filteredDrafts.map((draft) => {
+        console.log(`Draft ID: ${draft.id}`);
+        console.log(`Date Selected: ${draft.dateSelected}`);
+        console.log(`Time Selected: ${draft.timeSelected}`);
+
+        const timeSelected = draft.timeSelected;
+        let formattedNewTime = "";
+        if (timeSelected) {
+          try {
+            const parsedTime = parse(timeSelected, "HH:mm", new Date());
+            const newTime = addHours(parsedTime, 1);
+            formattedNewTime = format(newTime, "HH:mm");
+          } catch (error) {
+            console.error("Error parsing time:", error);
+          }
+        }
+
+        const dateSelected = draft.dateSelected;
+        let formattedDate = "";
+        if (dateSelected) {
+          try {
+            const parsedDate = parse(dateSelected, "M/d/yyyy", new Date());
+            formattedDate = format(parsedDate, "dd MMMM");
+          } catch (error) {
+            console.error("Error parsing date:", error);
+          }
+        }
+
+        return (
+          <View key={draft.id} style={styles.draftContainer}>
+            <View style={styles.nameReference}>
+              <View style={styles.iconContainer}>
+                <MaterialCommunityIcons
+                  name="hair-dryer"
+                  size={24}
+                  color="black"
+                />
+              </View>
+              <View>
+                <Text style={styles.draftTitle}>{draft.title}</Text>
+                <Text>Reference Code: #D-{draft.id.slice(0, 5)}</Text>
+              </View>
+            </View>
+            <View style={styles.statusContainer}>
+              <Text style={styles.statusText}>Status</Text>
+              <Text style={[styles.status, getStatusStyles(draft.status)]}>
+                {draft.status}
+              </Text>
+            </View>
+
+            <View style={styles.scheduleContainer}>
+              <View style={styles.containerIcon}>
+                <Feather name="calendar" size={24} color="black" />
+              </View>
+              <View style={styles.timeSpace}>
+                <Text style={styles.containerTime}>
+                  {formattedNewTime}, {formattedDate}{" "}
+                </Text>
+                <Text style={styles.statusText}>Schedule</Text>
+              </View>
+            </View>
+
+            <View style={styles.BContainer}>
+              <View style={styles.containerIcon}>
+                <MaterialCommunityIcons
+                  name="crosshairs-gps"
+                  size={24}
+                  color="#00B1F7"
+                />
+              </View>
+              <View style={styles.timeSpace}>
+                <Text style={styles.containerTime}>{draft.business}</Text>
+                <Text style={styles.statusText}>Service provider</Text>
+              </View>
+
+              <Button
+                title="Call"
+                onPress={() => {
+                  const phoneNumber = "4399899855";
+                  const phoneUrl = `tel:${phoneNumber}`;
+                  Linking.openURL(phoneUrl);
+                }}
               />
             </View>
-            <View>
-              <Text style={styles.draftTitle}>{draft.title}</Text>
-              <Text>Reference Code: #D-{draft.id.slice(0, 5)}</Text>
-            </View>
           </View>
-          <View style={styles.statusContainer}>
-            <Text style={styles.statusText}>Status</Text>
-            <Text style={[styles.status, getStatusStyles(draft.status)]}>
-              {draft.status}
-            </Text>
-          </View>
-          <Text>
-            Time: {draft.timeSelected} - {parseInt(draft.timeSelected) + 1}:00
-          </Text>
-          <Text>Date: {draft.dateSelected}</Text>
-          <Text>Business: {draft.business}</Text>
-        </View>
-      ))}
+        );
+      })}
     </>
   );
 };
@@ -104,6 +164,35 @@ const styles = StyleSheet.create({
   status: {
     padding: 5,
     borderRadius: 5,
+  },
+  containerIcon: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#E5E5E5",
+  },
+  scheduleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  BContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginTop: 10,
+  },
+  containerTime: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  timeSpace: {
+    justifyContent: "flex-start",
+    height: 50,
   },
 });
 
